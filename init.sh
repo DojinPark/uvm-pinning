ad2ef9472b9a0128d4d0b389ffd8e86754a6d194
DojinPark <74354737+DojinPark@users.noreply.github.com>
2021-04-05 15:40:03 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,21 @@
-# Edit DRIVER_PATH to your extracted driver path
+#!/bin/bash
+
+# Edit DRIVER_PATH and UVM_PINNING_PATH to your extracted driver path
 export DRIVER_PATH="./NVIDIA-Linux-x86_64-450.51.06"
-export CUDA_PATH="/usr/local/cuda"
+export UVM_PINNING_PATH="~/uvm-pinning/"
+export CUDA_PATH="/usr/local/cuda/"
 export PATH="$CUDA_PATH/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
 export CPATH="$CUDA_PATH/include:$CPATH"
-export CPP_INCLUDE_PATH="$CPP_INCLUDE_PATH:/usr/local/include/eigen3" # Eigen
+export CPP_INCLUDE_PATH="$CPP_INCLUDE_PATH:/usr/local/include/eigen3/"
 
-# Logging utils
+# Logging utililty function
 export UVM_PINNING_PREFIX='uvm-pinning'
+export $PATH="$PATH:$UVM_PINNING_PATH/logs/"
 function uvm-pinning-log {
 	echo > /dev/null | sudo tee /var/log/kern.log # Cleanse other kernel logs
 	cp /var/log/kern.log $1
 	sed -i "/.*nvidia-uvm:.*/d" $1
 	sed -i "s/^.*\($UVM_PINNING_PREFIX:\)//g" $1
-	plot $1
+	uvm-pinning-plot $1
 }
-
-
